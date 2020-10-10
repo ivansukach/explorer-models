4ad3e75db0181b9301cffdbd8d62af307ec0aa1b
Ivan Sukach <vsukach1999@gmail.com>
2020-10-10 17:28:34 +0300

    Fields NumTxs and TotalTxs have been added to Block struct

--- entities/block.go
+++ entities/block.go
@@ -11,6 +11,8 @@ type Block struct {
 	BlockHash     string    `json:"block_hash" sql:",notnull,unique"`
 	ParentHash    string    `json:"parent_hash" sql:",notnull"`
 	NumPrecommits int64     `json:"num_pre_commits" sql:",notnull"`
+	NumTxs        int64     `json:"num_txs" sql:"default:0"`
+	TotalTxs      int64     `json:"total_txs" sql:"default:0"`
 	Timestamp     time.Time `json:"timestamp" sql:"default:now()"`
 }
 
